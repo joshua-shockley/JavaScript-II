621bc5c8716f0c0822bc1ffd77bcc7d9afc61215
Joshua-Shockley <thelumbashockley@gmail.com>
2019-07-23 22:13:45 -0500

    end of the night after hours closed..i give up for the day

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -58,23 +58,31 @@ const runners = [{ "id": 1, "first_name": "Charmain", "last_name": "Seiler", "em
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
 
-
-
+runners.forEach(x => fullName.push(`${x.first_name}, ${x.last_name}`))
+console.log(fullName);
 
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
-let allCaps = [];
+let allCaps = runners.map(runner => runner.first_name.toUpperCase())
+    //runner is the made up variable to be a placeholder to help set next part of ////method to rest of the function runner could be x instead
 console.log(allCaps);
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
-let largeShirts = [];
+let largeShirts = runners.filter(runners => runners.shirt_size == "L")
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
-let ticketPriceTotal = [];
+let ticketPriceTotal = []
+
+function add(runners) {
+    return runners.reduce(funciton(prev, curr, i, fullArr));
+    return prev + curr;
+}
+ticketPriceTotal.push(add());
+//not working yet.
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
